a9d9227c75f43435b6620e73c1614f693e206626
yoonhang <com717942@gmail.com>
2024-09-04 11:52:52 +0900

    1

---
+++ README.md
@@ -0,0 +1,34 @@
+# 프로젝트 시작 
+npm init
+
+# dependency 설치 
+npm install [dependency 이름]
+- express: 백엔드 프레임워크 
+- cors: cors 설정 
+- json: json 파싱 
+- body-parser: 요청 body 받기 
+- nodemon: index.js 저장시 서버 새로고침
+
+# api 테스트
+- postman 
+- REST Client (vs code extension)
+
+# database 
+- 계정 및 database 생성 (root)
+```
+create database db_codingrecipe;
+create user user_codingrecipe@localhost identified by '1234';
+grant all privileges on db_codingrecipe.* to user_codingrecipe@localhost;
+```
+- 사용할 table
+```
+drop table if exists nations_table;
+create table nations_table(
+	id bigint auto_increment primary key,
+    name varchar(20),
+    capital varchar(20),
+    population int
+    );
+
+insert into nations_table(name, capital, population) values(?,?,?);    
+```
---
+++ index.js
@@ -0,0 +1,116 @@
+import express from "express";
+import mysql from "mysql";
+
+const app = express();
+// json 데이터 받기 위해 
+app.use(express.json());
+
+// db 접속 정보 
+const db = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    port: "3306",
+    database: "db_codingrecipe"
+});
+
+db.connect(err => {
+    console.log("db 연결 성공!!");
+    console.log("err", err);
+});
+
+// 서버포트: 8000 
+// localhost:8000
+app.listen(8000, () => {
+    console.log("8000 서버 호출");
+});
+
+// 기본 주소 요청 
+// localhost:8000/
+app.get("/", () => {
+    console.log("기본주소 요청");
+});
+
+app.get("/hello", () => {
+    console.log("hello 주소 요청");
+});
+
+// query string 받기
+app.get("/qs", (req) => {
+    console.log(req.query);
+    console.log(req.query.p1);
+    console.log(req.query.p2);
+});
+
+app.post("/hello", () => {
+    console.log("/hello post 요청");
+});
+
+app.post("/post-req", (req) => {
+    console.log(req.body);
+    console.log(req.body.name);
+    const { name, age } = req.body;
+    console.log(`name: ${name}, age: ${age}`);
+});
+
+app.post("/save", (req) => {
+    const { name, capital, population } = req.body;
+    console.log(`name: ${name}, capital: ${capital}, population: ${population}`);
+    const sql = "insert into nations_table(name, capital, population) values(?,?,?)";
+    db.query(sql, [name, capital, population], (err, results, fields) => {
+        console.log("err", err);
+        console.log("results", results);
+        console.log("fields", fields);
+    });
+});
+
+app.get("/list", (req, res) => {
+    const sql = "select * from nations_table";
+    db.query(sql, (err, results, fields) => {
+        console.log("err", err);
+        console.log("results", results);
+        console.log("fields", fields);
+        res.json(results);
+    });
+});
+
+app.get("/:id", (req, res) => {
+    console.log(req.params.id);
+    const id = req.params.id;
+    const sql = "select * from nations_table where id=?";
+    db.query(sql, [id], (err, results, fields) => {
+        console.log("err", err);
+        console.log("results", results);
+        if(results.length == 0) {
+            // 조회 결과 없음. 
+            res.status(404).send("요청하신 데이터를 찾을 수 없습니다.");
+        } else {
+            // 조회 결과 있음. 
+            res.status(200).json(results);
+
+        }
+    });
+});
+
+app.put("/:id", (req, res) => {
+    const { id, name, capital, population } = req.body;
+    console.log(`id: ${id}, name: ${name}, capital: ${capital}, population: ${population}`);
+    const sql = "update nations_table set population=? where id=?";
+    db.query(sql, [population, id], (err, results, fields) => {
+        console.log("err", err);
+        console.log("results", results);
+        console.log("fields", fields);
+        res.status(200);
+    });
+});
+
+app.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "delete from nations_table where id=?";
+    db.query(sql, [id], (err, results, fields) => {
+        console.log("err", err);
+        console.log("results", results);
+        console.log("fields", fields);
+
+    });
+});
